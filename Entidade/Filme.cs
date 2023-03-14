5a6150aa686d2a01aaddf0c3c56ebe99aa5ea992
Andréa <andreamaragao@gmail.com>
2023-03-13 22:32:27 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Filme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto02.Entidade
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+
+        public int Duracao { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Projeto02.Entidade;
+using System;
+
+namespace Projeto02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filme filme1;
+            filme1 = new Filme();
+            filme1.Titulo = "avatar";
+            filme1.Duracao = 190;
+
+            Filme filme2;
+            filme2 = new Filme();
+            filme2.Titulo = "dinossauro";
+            filme2.Duracao = 120;
+
+            Filme filme3;
+            filme3 = new Filme();
+            filme3.Titulo = "fuga";
+            filme3.Duracao = 90;
+
+
+        }
+    }
+}
